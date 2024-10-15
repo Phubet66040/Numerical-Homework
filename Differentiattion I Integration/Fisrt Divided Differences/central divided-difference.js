9f1e9f0cbe098d4c4b699fc5a80b2dca0b471a95
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2024-10-16 02:43:58 +0700

    19
    นอนจริงละ

--- Differentiattion I Integration/Fisrt Divided Differences/central divided-difference.js
+++ Differentiattion I Integration/Fisrt Divided Differences/central divided-difference.js
@@ -25,9 +25,21 @@ let diffff = (x)=>{
 }
 
 let s1 = math.derivative('exp(x)', 'x').evaluate({ x: x });
-let es = math.abs((dif(x) - s1) / dif(x)) * 100 / 100;
-console.log("error : "+es+"\n");
-console.log("diff1 : "+dif(x));
-console.log("diff2 : "+diff(x));
-console.log("diff3 : "+difff(x));
-console.log("diff4 : "+diffff(x));
+let s2 = math.derivative(('exp(x)', 'x'),'x').evaluate({ x: x });
+let s3 = math.derivative((('exp(x)', 'x'),'x'),'x').evaluate({ x: x });
+let s4 = math.derivative(((('exp(x)', 'x'),'x'),'x'),'x').evaluate({ x: x });
+
+
+let es1 = math.abs((dif(x) - s1) / dif(x)) * 100 / 100;
+let es2 = math.abs((diff(x) - s2) / dif(x)) * 100 / 100;
+let es3 = math.abs((difff(x) - s3) / dif(x)) * 100 / 100;
+let es4 = math.abs((diffff(x) - s4) / dif(x)) * 100 / 100;
+
+console.log("error : "+es1);
+console.log("diff1 : "+dif(x)+"\n");
+console.log("error : "+es2);
+console.log("diff2 : "+diff(x)+"\n");
+console.log("error : "+es3);
+console.log("diff3 : "+difff(x)+"\n");
+console.log("error : "+es4);
+console.log("diff4 : "+diffff(x)+"\n");
