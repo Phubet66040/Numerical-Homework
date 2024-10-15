1c7093df93d6298e0fec47c9e81476980444e6de
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2024-10-16 01:06:07 +0700

    13
    single

---
+++ Differentiattion I Integration/SingleTrapezoidal.js
@@ -0,0 +1,11 @@
+const math =  require('mathjs');
+let first = 2;//ค่าล่างของอินทิกเรต
+let last = 8;//ค่าบนของอินทริเกต
+let func = (x)=>{//สู๖รอินทีเกรดในการบ้าน
+    return 4 * math.pow(x,5) - 3 * math.pow(x,4) + math.pow(x,3) - 6 * x + 2;
+
+}
+
+let I = ((last-first)/2)*(func(first)+func(last));
+
+console.log("is : "+I);
