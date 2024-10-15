859aadbc7421a7e1d50ecfa8f432f5b83cbc27a8
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2024-10-16 01:46:17 +0700

    15

--- Differentiattion I Integration/Simpson's.js
+++ Differentiattion I Integration/Simpson's.js
@@ -1,1 +1,12 @@
 const math = require('mathjs');
+
+let first =  -1;
+let last = 2;
+let h = (last-first)/2;
+let func = (x)=>{
+    return math.pow(x,7)+2*math.pow(x,3)-1;
+}
+
+let I = (h/3)* (func(first) + 4 * func(first+h) + func(last));// h/3*1f(x0)+4f(x1)+1f(x2)
+
+console.log("is : "+I);
