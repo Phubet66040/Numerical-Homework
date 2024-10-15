2f7dfe54ab519a8b82e4aff152c28cd6b43c3b99
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2024-10-15 20:35:27 +0700

    5

--- Spiline.js
+++ Spiline.js
@@ -2,13 +2,24 @@ const math = require('mathjs');
 
 // f1(x) = f(x0)+mi*(x-x0) : mi = f(x2)-f(x1)/(x2-x1);
 //x0 <= x >= x1;
+//let y = f(x)
 
 let x = [2,4,6,8,10];
-let y = [9.5,8.0,10.5,39.5,72.5];
+let y = [9.5,8.0,10.5,39.5,72.5];//let y = f(x)
 let z = 4.5;
 let m = [];//make array empty
 
+
 for(let i =0;i<x.length;i++){
-    m.push = (y[i+1]-y[i])/(x[i+1]-x[i]);
-    console.log(m[i]);
+    m.push((y[i+1]-y[i])/(x[i+1]-x[i]));//mi = f(x2)-f(x1)/(x2-x1);
+    console.log(m);
+}
+
+
+for(let j = 0;j < x.length;j++){
+    if(z >= x[j] && z <= x[j+1]){//x0 <= x >= x1;
+        let aws = y[j]+(m[j]*(z-x[j]));// f1(x) = f(x0)+mi*(x-x0)
+        console.log(aws);
+        
+    }
 }
