8863a2231da5667d545557644c4ba7b1f286b55e
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2024-10-15 20:10:11 +0700

    3

--- Lagrange.js
+++ Lagrange.js
@@ -1,15 +1,17 @@
 const math = require('mathjs');
 
-let x = [0,20000,40000,60000,80000];
-let y = [9.81,9.7487,9.6879,9.6879,9.5682];
+let x = [0,20000,40000,60000,80000];//linear [0,80000];  quadtatic : [0,40000,80000];  polynomail: [0,20000,40000,60000,80000];
+let y = [9.81,9.7487,9.6879,9.6879,9.5682];//linear [9.81,9.5682];  quadtatic : [9.81,9.6879,9.5682];  polynomail: [9.81,9.7487,9.6879,9.6879,9.5682];
 let z = new Array(x.length).fill(0);
 let value = 42000;
 let aws = 0;
+
+
 //     f(x) = (x1-x)/(x1-x0)*f(x0) + (x0-x)/(x0-x1)*f(x1)
 for(let i = 0;i<x.length;i++){
-    let first = 1;
-    let second = 1;
-    for(int j = 0;j<x.length;j++){
+    let first = 1;//บรรทัดแรก
+    let second = 1;//บรรทัดสอง
+    for(let j = 0;j<x.length;j++){
         if(j!=i){
             first *=(x[j]-value);
             second *=(x[j]-x[i]);
@@ -18,9 +20,11 @@ for(let i = 0;i<x.length;i++){
     }
     z[i] = first/second;
 }
+
+//รวมและหาคำตอบที่ได้
 for(let k =0;k<x.length;k++){
     aws +=(z[k]*y[k]);
 }
 
 
-console.log(z);
+console.log(aws);
