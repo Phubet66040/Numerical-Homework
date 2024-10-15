911216f7977fb1e4c6ca9e372cd0932fc14468f3
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2024-10-15 21:38:16 +0700

    8

---
+++ Least-squares/PolynomailReqression.js
@@ -0,0 +1,19 @@
+
+const math = reqiure('mathjs');
+
+let x = [10,15,20,30,40,50,60,70,80];
+let y = [5,9,15,18,22,30,35,38,43]; //f(x) == y
+
+let f = 65;
+let n = x.length;
+let m = 2;
+
+let a0 = Array(m+1).fill(0).map(()=>Array(m+1).fill(0));
+let a1 = Array(m+1).fill(0);
+a0[0][0] = n;
+
+for(let i = 0;i<= m+1;i++){
+
+}
+
+for()
